dc01b94de0ffd7abb4415b0d3793d96ae3e895db
Rubén Montoro <rumont98@gmail.com>
2023-06-04 11:06:27 +0200

    upload

---
+++ removeYoutubeShortsButton.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Remove Shorts button
+// @namespace    https://www.youtube.com/*
+// @version      0.1
+// @description  removes shorts button and kicks you out from the shorts page
+// @author       Rumont
+// @match        https://www.youtube.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  'use strict';
+  const kickFromShortsPage = () => {
+    if (window.location.href.includes('shorts')) window.location.replace("https://www.youtube.com");
+  }
+
+  window.addEventListener('yt-page-type-changed', (e) => {
+    if (document.querySelector('a[title="Shorts"]') != null) document.querySelector('a[title="Shorts"]').remove();
+    kickFromShortsPage();
+  });
+
+  if (document.querySelector('a[title="Shorts"]') != null) document.querySelector('a[title="Shorts"]').remove();
+  kickFromShortsPage();
+})();
